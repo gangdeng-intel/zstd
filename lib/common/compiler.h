6a477fb0b734e89a299e550b2f29b5cb5220f441
gangdeng-intel <gang.deng@intel.com>
2026-03-17 14:05:36 +0800

    Add compiler version gating for APX target attributes
    Safely fallback to BMI2 for Clang versions < 20 and GCC versions < 14, to prevent older compilers from rejecting or dropping unrecognized target strings.

--- lib/common/compiler.h
+++ lib/common/compiler.h
@@ -135,9 +135,19 @@
  * Includes BMI2 features as well.
  */
 #ifdef __clang__
-#  define APXF_TARGET_ATTRIBUTE TARGET_ATTRIBUTE("egpr,push2pop2,ppx,ndd,ccmp,nf,cf,zu,lzcnt,bmi,bmi2")
+#  if __clang_major__ >= 20
+#    define APXF_TARGET_ATTRIBUTE TARGET_ATTRIBUTE("egpr,push2pop2,ppx,ndd,ccmp,nf,cf,zu,lzcnt,bmi,bmi2")
+#  else
+#    define APXF_TARGET_ATTRIBUTE TARGET_ATTRIBUTE("lzcnt,bmi,bmi2")
+#  endif
+#elif defined(__GNUC__)
+#  if __GNUC__ >= 14
+#    define APXF_TARGET_ATTRIBUTE TARGET_ATTRIBUTE("apxf,lzcnt,bmi,bmi2")
+#  else
+#    define APXF_TARGET_ATTRIBUTE TARGET_ATTRIBUTE("lzcnt,bmi,bmi2")
+#  endif
 #else
-#  define APXF_TARGET_ATTRIBUTE TARGET_ATTRIBUTE("apxf,lzcnt,bmi,bmi2")
+#  define APXF_TARGET_ATTRIBUTE TARGET_ATTRIBUTE("lzcnt,bmi,bmi2")
 #endif
 
 /* prefetch
